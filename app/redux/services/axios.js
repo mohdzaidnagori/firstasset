1fd5e95f7f293d735d552b68442fa8ec6ed9c387
mohd zaid <mohd@skilliza.com>
2023-09-15 19:39:12 +0530

    url ssl certified

--- app/redux/services/axios.js
+++ app/redux/services/axios.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 // Set your base URL or any other default configurations
 // axios.defaults.baseURL = 'https://www.skilliza.com/wscubetech/public/api/user/';
 // axios.defaults.baseURL = 'http://127.0.0.1:8000/api/user/';
-axios.defaults.baseURL = 'http://api.firstasset.in/api/user/';
+axios.defaults.baseURL = 'https://api.firstasset.in/api/user/';
 
 export default axios;
