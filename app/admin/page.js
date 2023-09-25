61e9942524241b71f95963883001df9cd59961de
mohd zaid <mohd@skilliza.com>
2023-09-25 18:02:39 +0530

    again update

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,12 +1,6 @@
-"use client"
-import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 const page = () => {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/admin/all_users')
-  },[])
   return (
     <div>loading...</div>
   )
