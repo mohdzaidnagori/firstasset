27dced9a3c2d4ec4deaaedec1a3bcf94a24ffefe
mohd zaid <mohd@skilliza.com>
2023-10-09 19:56:28 +0530

    banner works

--- components/swiper/homeBanner/Banner.jsx
+++ components/swiper/homeBanner/Banner.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { EffectCreative, Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/effect-creative';
@@ -14,7 +14,7 @@ const imgData = [
     {
         id: 1,
         img: "/banner/Home_Page_Pic_1.jpg",
-        head:''
+        head: ''
     },
     {
         id: 2,
@@ -25,7 +25,7 @@ const imgData = [
         id: 3,
         img: "/banner/pexels-rdne-stock-project-8293700.jpg",
         head: 'MANDATED PROJECTS',
-        height:''
+        height: ''
     },
     {
         id: 4,
@@ -37,46 +37,61 @@ const imgData = [
 
 
 const Banner = () => {
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+
+    console.log(imagesLoaded)
     return (
         <div className='absolute w-full h-full inset-0'>
-            <Swiper
-                grabCursor={true}
-                effect={'creative'}
-                loop={true}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
-                creativeEffect={{
-                    prev: {
-                        shadow: false,
-                        translate: [0, 0, -400],
-                    },
-                    next: {
-                        translate: ['100%', '0%', '0%'],
-                    },
-                }}
-                modules={[Autoplay, EffectCreative]}
-                className="mySwiper w-full h-full"
-            >
-                {
-                    imgData.map((item,index) => (
-                        <SwiperSlide key={item.id} className={`${style.swiperSlide} relative`}>
-                            <Image fill={true} sizes='100%' src={item.img} alt='banner images'  style={{ objectFit: 'cover' }} />
-                            {
-                                item.head !== '' &&
-                                <>
-                                    <div className={`bg-gray-900 absolute top-[50%] translate-y-[-50%] ${index === 1 ? 'h-[40%]' : 'h-[30%]'} left-4 sm:w-[40%] w-[80%] opacity-20`} />
-                                    <div className='absolute top-[50%] translate-y-[-50%] left-4 text-white sm:w-[40%] w-[80%] h-[50%] flex justify-start items-center'>
-                                        <h1 className='sm:text-4xl md:text-5xl text-3xl uppercase ml-8 leading-relaxed'>{item?.head}</h1>
-                                    </div>
-                                </>
-                            }
-                        </SwiperSlide>
-                    ))
-                }
+            {(
+                <Swiper
+                    grabCursor={true}
+                    effect={'creative'}
+                    loop={true}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    creativeEffect={{
+                        prev: {
+                            shadow: false,
+                            translate: [0, 0, -400],
+                        },
+                        next: {
+                            translate: ['100%', '0%', '0%'],
+                        },
+                    }}
+                    modules={[Autoplay, EffectCreative]}
+                    className="mySwiper w-full h-full"
+                >
+                    {
+                        imgData.map((item, index) => (
+                            <SwiperSlide key={item.id} className={`${style.swiperSlide} relative`}>
+                                <div className="placeholder" style={{ display: 'none' }}>
+                                    placeholer loading
+                                </div>
+                                <Image
+                                    fill={true}
+                                    sizes='100%'
+                                    src={item.img}
+                                    onLoadingComplete={() => setImagesLoaded(true)}
+                                    style={{ objectFit: 'cover' }}
+                                    alt='banner images'
+                                />
+                                {
+                                    (item.head !== '' || imagesLoaded ) &&
+                                    <>
+                                        <div className={`bg-gray-900 absolute top-[50%] translate-y-[-50%] ${index === 1 ? 'h-[40%]' : 'h-[30%]'} left-4 sm:w-[40%] w-[80%] opacity-20`} />
+                                        <div className='absolute top-[50%] translate-y-[-50%] left-4 text-white sm:w-[40%] w-[80%] h-[50%] flex justify-start items-center'>
+                                            <h1 className='sm:text-4xl md:text-5xl text-3xl uppercase ml-8 leading-relaxed'>{item?.head}</h1>
+                                        </div>
+                                    </>
+                                }
+                            </SwiperSlide>
+                        ))
+                    }
 
-            </Swiper>
+                </Swiper>
+            )}
         </div>
     )
 }
